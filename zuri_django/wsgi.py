51ce6ca31b898beb68b3b1d884186e89ce1fd14f
KoredeDavid <koredeoluwashola@gmail.com>
2021-05-07 11:12:34 +0100

    I am tired of this shit

--- zuri_django/wsgi.py
+++ zuri_django/wsgi.py
@@ -16,3 +16,8 @@ if os.environ.get('DJANGO_ENV', '') == 'production':
 else:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zuri_django.settings.dev')
 application = get_wsgi_application()
+
+if os.environ.get('DJANGO_ENV', '') == 'production':    
+    from whitenoise.django import DjangoWhiteNoise
+    application = DjangoWhiteNoise(application)
+  
